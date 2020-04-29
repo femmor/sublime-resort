637dd4356946664cb367af022c936194afb3a539
Emmanuel Egomson <femmor2u@gmail.com>
2020-04-29 16:09:04 -0500

    finished the single room component

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import defaultBcg from "../images/room-1.jpeg"
-import Hero from "../components/Hero"
+// import Hero from "../components/Hero"
 import Banner from "../components/Banner"
 import { Link } from "react-router-dom"
 import { RoomContext } from "../context"
@@ -9,14 +9,6 @@ import { RoomContext } from "../context"
 import StyledHero from "../components/StyledHero"
 
 class SingleRoom extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     // console.log(this.props)
-    //     this.state = {
-    //         slug: this.props.match.params.slug,
-    //         defaultBcg
-    //     }
-    // }    
 
     state = {
         slug: this.props.match.params.slug,
@@ -37,13 +29,43 @@ class SingleRoom extends Component {
 
         console.log(room)
         const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
+        const [ mainImg, ...defaultImg ] = images
         return (
             <>
-             <StyledHero img={images[0] || this.state.defaultBcg}>
+             <StyledHero img={mainImg || this.state.defaultBcg}>
                 <Banner title={`${name} room`}>
                     <Link to="/rooms" className="btn-primary">back to rooms</Link>
                 </Banner>
              </StyledHero>
+            <section className="single-room">
+                <div className="single-room-images">
+                    {defaultImg.map((image, index) => {
+                        return <img key={index} src={image} alt={name} id="roomImg" />
+                    })}
+                </div>
+                <div className="single-room-info">
+                    <article className="desc">
+                        <h3>details</h3>
+                        <p>{description}</p>
+                    </article>
+                    <article className="info">
+                        <h3>info</h3>
+                        <h6>price : ${price}</h6>
+                        <h6>size : {size} SQFT</h6>
+                        <h6>max capacity : {capacity} {capacity > 1 ? "people" : "person"}</h6>
+                        <h6>{breakfast ? "free breakfast included" : "No breakfast included"}</h6>
+                        <h6>{pets ? "Pets allowed" : "No pets allowed"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className="room-extras">
+                <h6>extras</h6>
+                <ul className="extras">
+                    {extras.map((item, index) => {
+                        return <li key={index}>- {item}</li>
+                    })}
+                </ul>
+            </section>
             </>
         );
     }
